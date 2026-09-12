e4d85ae7e750dc8651f9da675aa7f99ea0d27364
Rogesson Barboza <rogessonb@gmail.com>
2026-09-12 16:26:33 -0300

    allow kubernetes bind in python services

--- targeting-service/app.py
+++ targeting-service/app.py
@@ -16,4 +16,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", "8003")))
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", "8003")))  # nosec B104 - required for Kubernetes traffic
